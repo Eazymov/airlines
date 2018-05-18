949e6e54ba0084274d7375ab3140fea67be67186
Eazymov <eazymov@mail.ru>
2018-05-19 00:25:11 +0500

    update

--- Airlines/src/airlines/LongestPathFinder.java
+++ Airlines/src/airlines/LongestPathFinder.java
@@ -1,23 +1,44 @@
+/*
+ * Copyright (c) 2018 Eduard Azymov
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
 package airlines;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author eazymov
  */
-
 public class LongestPathFinder {
     City firstCity;
     WaysList waysList;
     List<City> cities;
 
     public List<City> find(
-        List<City> cities,
-        WaysList waysList,
-        City firstCity
+            List<City> cities,
+            WaysList waysList,
+            City firstCity
     ) {
         this.cities = cities;
         this.waysList = waysList;
@@ -29,63 +50,60 @@ public class LongestPathFinder {
         return this.getLongestPath(correctPaths);
     }
 
-    public List<List<City>> getAllPaths(List<City> arr) {
-        if (arr.size() > 1) {
-            City beg = arr.get(0);
-            List<List<City>> supArr = this.getAllPaths(arr.subList(1, arr.size()));
-            List<List<City>> subArr = new ArrayList<>();
-
-            for (int i = 0; i < supArr.size(); i++) {
-                List<City> childArr = supArr.get(i);
-
-                for (int j = 0; j <= childArr.size(); j++) {
-                    List<City> temp = supArr.get(i).subList(0, j);
-                    int supArrSize = supArr.size();
-                    
-                    temp.add(beg);
-                    if (supArrSize > j) {
-                        temp.addAll(supArr.get(i).subList(j, supArrSize));
-                    }
-
-                    subArr.add(temp);
-                }
-            }
-
-            return subArr;
-        } else {
+    private List<List<City>> getAllPaths(List<City> original) {
+        if (original.isEmpty()) {
             List<List<City>> result = new ArrayList<>();
 
-            result.add(arr);
+            result.add(new ArrayList<>());
 
             return result;
         }
+
+        City firstElement = original.remove(0);
+        List<List<City>> returnValue = new ArrayList<>();
+        List<List<City>> permutations = getAllPaths(original);
+
+        permutations.stream().forEach(smallerPermutated -> {
+            for (int idx = 0; idx <= smallerPermutated.size(); idx++) {
+                List<City> temp = new ArrayList<>(smallerPermutated);
+
+                temp.add(idx, firstElement);
+                returnValue.add(temp);
+            }
+        });
+
+        return returnValue;
     }
-    
+
     private List<City> getLongestPath(
-        List<List<City>> paths
+            List<List<City>> paths
     ) {
         List<City> longestPath = new ArrayList<>();
-        
+
         for (int idx = 0; idx < paths.size(); idx++) {
             List<City> curPath = paths.get(idx);
-            
+
             if (curPath.size() > longestPath.size()) {
                 longestPath = curPath;
             }
         }
-        
+
         return longestPath;
     }
 
     public List<List<City>> getCorrectPaths(List<List<City>> paths) {
         List<List<City>> correctPaths = new ArrayList<>();
-        
+
         for (int idx = 0; idx < paths.size(); idx++) {
             List<City> path = paths.get(idx);
-            
-            if (path.size() == 0) continue;
 
-            if (path.get(0) != firstCity) continue;
+            if (path.isEmpty()) {
+                continue;
+            }
+
+            if (path.get(0) != firstCity) {
+                continue;
+            }
 
             List<City> correctedPath = this.correctPath(path);
             City lastCity = correctedPath.get(correctedPath.size() - 1);
@@ -95,21 +113,23 @@ public class LongestPathFinder {
                 correctPaths.add(correctedPath);
             }
         }
-        
+
         return correctPaths;
     }
-    
+
     private List<City> correctPath(List<City> path) {
         int size = path.size();
         int breakPoint = size;
-        
+
         for (int idx = 1; idx < size; idx++) {
             City currentCity = path.get(idx);
             City previousCity = path.get(idx - 1);
 
             boolean noWay = !this.waysList.hasWay(previousCity, currentCity);
 
-            if (noWay) breakPoint = idx;
+            if (noWay) {
+                breakPoint = idx;
+            }
         }
 
         return path.subList(0, breakPoint);
